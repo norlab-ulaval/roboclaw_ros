f77cf9c8f0de38645f51f6b67aa98d557b78a419
norlab-marmotte <norlabmarmotte@gmail.com>
2024-04-04 17:28:38 -0400

    clean up launch fil and created config files

--- launch/roboclaw_node_launch.py
+++ launch/roboclaw_node_launch.py
@@ -1,29 +1,25 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from launch import LaunchDescription
-
-
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='roboclaw_node_ros',
-            executable='roboclaw_node',
-            name='roboclaw_node',
-            parameters=[
-                {'dev': '/dev/serial/by-id/usb-Basicmicro_Inc._USB_Roboclaw_2x30A-if00'},
-                {'baud': 115200},
-                {'address': 128},
-                {'max_speed': 2.0},
-                #{'~ticks_per_meter': 4342.2},
-                {'ticks_per_meter': 3802.4},
-                {'ticks_per_rotation': 2780},
-                {'base_width': 0.315},
-                {'pub_odom': True},
-                {'pub_elec': True},
-                {'stop_movement': True},
-            ],
-            remappings=[
-                ('/cmd_vel', '/twist_mux/cmd_vel'),
-            ]
-        )
-    ])
+
+    ld = LaunchDescription()
+    config_folder = os.path.join(get_package_share_directory('roboclaw_node_ros'), 'config')
+
+    # Roboclaw node
+    roboclaw_node = Node(
+        package='roboclaw_node_ros',
+        executable='roboclaw_node',
+        name='roboclaw_node',
+        parameters = [
+            os.path.join(config_folder, 'roboclaw.yaml'),
+        ],
+        remappings=[
+            ('/cmd_vel', '/twist_mux/cmd_vel'),
+        ]
+    )
+    ld.add_action(roboclaw_node)
+
+    return ld
