ca785a01497d591c8ff947de25f032cd96566ef8
Damien LaRocque <phicoltan@gmail.com>
2024-04-10 14:53:55 -0400

    Review node

--- roboclaw_node_ros/roboclaw_node.py
+++ roboclaw_node_ros/roboclaw_node.py
@@ -32,10 +32,14 @@ class RoboclawNode(Node):
 
         self.updater = diagnostic_updater.Updater(self)
         self.updater.setHardwareID("RoboClaw")
-        self.updater.add(diagnostic_updater.FunctionDiagnosticTask("Vitals", self.diagnostics))
+        self.updater.add(
+            diagnostic_updater.FunctionDiagnosticTask(
+                "Vitals",
+                self.diagnostics,
+            )
+        )
 
         self.last_cmd_timestamp = self.get_clock().now().nanoseconds
-
 
     def read_parameters(self):
         """Read parameters from the parameter server"""
@@ -78,14 +82,12 @@ class RoboclawNode(Node):
 
         # TODO: add a parameter for the acceleration (tried but it didn't work)
 
-
     def init_parameter(self, name, default):
         """Initialize a parameter and log it"""
 
         value = self.declare_parameter(name, default).value
         self.get_logger().info(f"  {name}: {value}")
         return value
-    
 
     def update_parameters(self):
         """Update dynamic parameters once in a while"""
@@ -100,19 +102,28 @@ class RoboclawNode(Node):
         self.I = self.get_parameter("i_constant").value
         self.D = self.get_parameter("d_constant").value
         self.qpps = self.get_parameter("qpps").value
-        self.configure_device()     
+        self.configure_device()
 
-
-    def init_device(self, dev_name, address, baud_rate):
+    def init_device(self, dev_name: str, address: int, baud_rate: int):
         """Initialize the Roboclaw device and connect to it"""
 
         try:
-            self.get_logger().info("Connecting to Roboclaw at " + dev_name + " with address " + str(address))
+            self.get_logger().info(
+                "Connecting to Roboclaw at "
+                + dev_name
+                + " with address "
+                + str(address)
+            )
             driver = Roboclaw(dev_name, baud_rate, address)
             driver.open()
             self.get_logger().info("Connected!")
         except Exception as e:
-            self.get_logger().fatal("Could not connect to Roboclaw at " + dev_name + " with address " + str(address))
+            self.get_logger().fatal(
+                "Could not connect to Roboclaw at "
+                + dev_name
+                + " with address "
+                + str(address)
+            )
             self.get_logger().debug(e)
             self.shutdown("Could not connect to Roboclaw")
 
@@ -124,7 +135,6 @@ class RoboclawNode(Node):
             self.get_logger().debug(e)
 
         return driver
-    
 
     def reset_device(self):
         """Reset the roboclaw speed and encoder counters"""
@@ -136,7 +146,6 @@ class RoboclawNode(Node):
             self.get_logger().warn("Device reset OSError: " + str(e.errno))
             self.get_logger().debug(e)
 
-    
     def configure_device(self):
         """Configure the Roboclaw device with the ROS parameters"""
 
@@ -145,12 +154,15 @@ class RoboclawNode(Node):
             self.driver.SetM1VelocityPID(self.P, self.I, self.D, self.qpps)
             self.driver.SetM2VelocityPID(self.P, self.I, self.D, self.qpps)
 
-
     def create_subscribers(self):
         """Create subscribers for the node"""
 
-        self.cmd_vel_sub = self.create_subscription(Twist, "/cmd_vel", self.cmd_vel_callback, 1)
-
+        self.cmd_vel_sub = self.create_subscription(
+            Twist,
+            "/cmd_vel",
+            self.cmd_vel_callback,
+            1,
+        )
 
     def create_wrappers(self):
         """Create the encoder and electrical wrappers"""
@@ -163,33 +175,40 @@ class RoboclawNode(Node):
             self.BASE_WIDTH,
             self.PUBLISH_ODOM,
             self.PUBLISH_ENCODERS,
-            self.PUBLISH_TF
+            self.PUBLISH_TF,
         )
         self.electrical_wrapper = ElectricalWrapper(
-            self,
-            self.driver,
-            self.PUBLISH_ELEC
+            self, self.driver, self.PUBLISH_ELEC
         )
-
 
     def create_timers(self):
         """Create timers for the node"""
 
-        if self.ODOM_RATE > 0:
-            self.odom_timer = self.create_timer(1.0 / self.ODOM_RATE, self.encoder_wrapper.update_and_publish)
-        if self.ELEC_RATE > 0:
-            self.elec_timer = self.create_timer(1.0 / self.ELEC_RATE, self.electrical_wrapper.update_and_publish)
-            
+        if self.ODOM_RATE:
+            self.odom_timer = self.create_timer(
+                1.0 / self.ODOM_RATE,
+                self.encoder_wrapper.update_and_publish,
+            )
+        if self.ELEC_RATE:
+            self.elec_timer = self.create_timer(
+                1.0 / self.ELEC_RATE,
+                self.electrical_wrapper.update_and_publish,
+            )
+
         self.idle_timer = self.create_timer(1 / 30, self.idle_callback)
         self.dynamic_params_timer = self.create_timer(1.0, self.update_parameters)
-
 
     def idle_callback(self):
         """Stop the robot if no commands are received for 'idle_timeout' seconds"""
 
         now = self.get_clock().now().nanoseconds
-        if (self.stop_if_idle and now - self.last_cmd_timestamp > self.idle_timeout * 1e9):
-            self.get_logger().info(f"Did not get command for {self.idle_timeout} second, stopping")
+        if (
+            self.stop_if_idle
+            and now - self.last_cmd_timestamp > self.idle_timeout * 1e9
+        ):
+            self.get_logger().info(
+                f"Did not get command for {self.idle_timeout} second, stopping"
+            )
             try:
                 self.driver.SpeedM1M2(0, 0)
                 self.last_cmd_timestamp = now
@@ -197,21 +216,32 @@ class RoboclawNode(Node):
                 self.get_logger().error("Could not stop")
                 self.get_logger().debug(e)
 
-
     def cmd_vel_callback(self, twist):
         """Callback for /cmd_vel topic, move the robot according to the Twist message"""
 
         self.last_cmd_timestamp = self.get_clock().now().nanoseconds
 
         # Limit the speed
-        linear_x = min(max(twist.linear.x, -self.max_speed_linear), self.max_speed_linear)
-        angular_z = min(max(twist.angular.z, -self.max_speed_angular), self.max_speed_angular)
+        linear_x = min(
+            max(twist.linear.x, -self.max_speed_linear),
+            self.max_speed_linear,
+        )
+        angular_z = min(
+            max(twist.angular.z, -self.max_speed_angular),
+            self.max_speed_angular,
+        )
 
         # Convert to motor speeds
-        right_speed = int((linear_x + angular_z * self.BASE_WIDTH) * self.TICKS_PER_METER)  # ticks/s
-        left_speed = int((linear_x - angular_z * self.BASE_WIDTH) * self.TICKS_PER_METER)
+        right_speed = int(
+            (linear_x + angular_z * self.BASE_WIDTH) * self.TICKS_PER_METER
+        )  # ticks/s
+        left_speed = int(
+            (linear_x - angular_z * self.BASE_WIDTH) * self.TICKS_PER_METER
+        )
 
-        self.get_logger().debug(f"Sending command -> right: {str(right_speed)}, left: {str(left_speed)} (ticks/sec)")
+        self.get_logger().debug(
+            f"Sending command -> right: {str(right_speed)}, left: {str(left_speed)} (ticks/sec)"
+        )
 
         # Send the command
         try:
@@ -219,7 +249,6 @@ class RoboclawNode(Node):
         except OSError as e:
             self.get_logger().warn("SpeedM1M2 OSError: " + str(e.errno))
             self.get_logger().debug(e)
-
 
     def diagnostics(self, stat):
         """Read the error status of the Roboclaw and report it as diagnostics"""
@@ -234,11 +263,10 @@ class RoboclawNode(Node):
         stat.summary(state, message)
         return stat
 
-
     # TODO: need clean shutdown so motors stop even if new msgs are arriving
     def shutdown(self, str_msg):
         """Shutdown the node"""
-        
+
         self.get_logger().info("Shutting down :" + str_msg)
         try:
             self.driver.SpeedM1M2(0, 0)
