bafc4c78fc0c09eca714e77d8d0a5ac6c9dbf7c3
Damien LaRocque <phicoltan@gmail.com>
2024-06-18 14:11:47 -0400

    Change RoboClaw error translation and add unit tests

--- roboclaw_node_ros/utils.py
+++ roboclaw_node_ros/utils.py
@@ -4,66 +4,32 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import Sequence, Tuple
 
-# Status Bit Mask : Function
-#     0x000000 : Normal
-#     0x000001 : E-Stop
-#     0x000002 : Temperature Error
-#     0x000004 : Temperature 2 Error
-#     0x000008 : Main Voltage High Error
-#     0x000010 : Logic Voltage High Error
-#     0x000020 : Logic Voltage Low Error
-#     0x000040 : M1 Driver Fault Error
-#     0x000080 : M2 Driver Fault Error
-#     0x000100 : M1 Speed Error
-#     0x000200 : M2 Speed Error
-#     0x000400 : M1 Position Error
-#     0x000800 : M2 Position Error
-#     0x001000 : M1 Current Error
-#     0x002000 : M2 Current Error
-#     0x010000 : M1 Over Current Warning
-#     0x020000 : M2 Over Current Warning
-#     0x040000 : Main Voltage High Warning
-#     0x080000 : Main Voltage Low Warning
-#     0x100000 : Temperature Warning
-#     0x200000 : Temperature 2 Warning
-#     0x400000 : S4 Signal Triggered
-#     0x800000 : S5 Signal Triggered
-#     0x01000000 : Speed Error Limit Warning
-#     0x02000000 : Position Error Limit Warning
-
 ROBOCLAW_ERRORS = {
-    0x0000: (DiagnosticStatus.OK, "Normal"),
-    0x0001: (DiagnosticStatus.WARN, "M1 over current"),
-    0x0002: (DiagnosticStatus.WARN, "M2 over current"),
-    0x0004: (DiagnosticStatus.ERROR, "Emergency Stop"),
-    0x0008: (DiagnosticStatus.ERROR, "Temperature1"),
-    0x0010: (DiagnosticStatus.ERROR, "Temperature2"),
-    0x0020: (
-        DiagnosticStatus.ERROR,
-        "Main batt voltage high",
-    ),
-    0x0040: (
-        DiagnosticStatus.ERROR,
-        "Logic batt voltage high",
-    ),
-    0x0080: (
-        DiagnosticStatus.ERROR,
-        "Logic batt voltage low",
-    ),
-    0x0100: (DiagnosticStatus.WARN, "M1 driver fault"),
-    0x0200: (DiagnosticStatus.WARN, "M2 driver fault"),
-    0x0400: (
-        DiagnosticStatus.WARN,
-        "Main batt voltage high",
-    ),
-    0x0800: (
-        DiagnosticStatus.WARN,
-        "Main batt voltage low",
-    ),
-    0x1000: (DiagnosticStatus.WARN, "Temperature1"),
-    0x2000: (DiagnosticStatus.WARN, "Temperature2"),
-    0x4000: (DiagnosticStatus.OK, "M1 home"),
-    0x8000: (DiagnosticStatus.OK, "M2 home"),
+    0x000000: (DiagnosticStatus.OK, "Normal"),
+    0x000001: (DiagnosticStatus.OK, "E-Stop"),
+    0x000002: (DiagnosticStatus.ERROR, "Temperature Error"),
+    0x000004: (DiagnosticStatus.ERROR, "Temperature 2 Error"),
+    0x000008: (DiagnosticStatus.ERROR, "Main Voltage High Error"),
+    0x000010: (DiagnosticStatus.ERROR, "Logic Voltage High Error"),
+    0x000020: (DiagnosticStatus.ERROR, "Logic Voltage Low Error"),
+    0x000040: (DiagnosticStatus.ERROR, "M1 Driver Fault Error"),
+    0x000080: (DiagnosticStatus.ERROR, "M2 Driver Fault Error"),
+    0x000100: (DiagnosticStatus.ERROR, "M1 Speed Error"),
+    0x000200: (DiagnosticStatus.ERROR, "M2 Speed Error"),
+    0x000400: (DiagnosticStatus.ERROR, "M1 Position Error"),
+    0x000800: (DiagnosticStatus.ERROR, "M2 Position Error"),
+    0x001000: (DiagnosticStatus.ERROR, "M1 Current Error"),
+    0x002000: (DiagnosticStatus.ERROR, "M2 Current Error"),
+    0x010000: (DiagnosticStatus.WARN, "M1 Over Current Warning"),
+    0x020000: (DiagnosticStatus.WARN, "M2 Over Current Warning"),
+    0x040000: (DiagnosticStatus.WARN, "Main Voltage High Warning"),
+    0x080000: (DiagnosticStatus.WARN, "Main Voltage Low Warning"),
+    0x100000: (DiagnosticStatus.WARN, "Temperature 1 Warning"),
+    0x200000: (DiagnosticStatus.WARN, "Temperature 2 Warning"),
+    0x400000: (DiagnosticStatus.WARN, "S4 Signal Triggered"),
+    0x800000: (DiagnosticStatus.WARN, "S5 Signal Triggered"),
+    0x01000000: (DiagnosticStatus.WARN, "Speed Error Limit Warning"),
+    0x02000000: (DiagnosticStatus.WARN, "Position Error Limit Warning"),
 }
 
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,24 @@
-from roboclaw_node_ros.utils import decipher_rclaw_status
+from roboclaw_node_ros.utils import ROBOCLAW_ERRORS, decipher_rclaw_status
+import pytest
 
 
-def test_byte_unpacking():
-    value = 491200
-    statuses = decipher_rclaw_status(value)
-    print(statuses)
-    # TODO: Add unit cases for assertions
+@pytest.fixture
+def marmotte_unit_statuses():
+    value = 0x40068001
+    return decipher_rclaw_status(value)
+
+
+def test_marmotte_stat_len(marmotte_unit_statuses):
+    assert len(marmotte_unit_statuses) == 3
+
+
+def test_marmotte_stat_estop(marmotte_unit_statuses):
+    assert ROBOCLAW_ERRORS[0x000001] in marmotte_unit_statuses
+
+
+def test_marmotte_stat_overcurrent_warn(marmotte_unit_statuses):
+    assert ROBOCLAW_ERRORS[0x020000] in marmotte_unit_statuses
+
+
+def test_marmotte_stat_overvoltage_warn(marmotte_unit_statuses):
+    assert ROBOCLAW_ERRORS[0x040000] in marmotte_unit_statuses
